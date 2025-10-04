d8f878dc00ae608d644dc51b9869eb3d443d2e3f
itsmebilly004 <wanyamabill4@gmail.com>
2025-10-04 06:43:37 +0300

    t

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// --- TEMPORARY DIAGNOSTIC LOGS ---
-// This will help us see what Vercel is sending to your application.
-console.log("VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("VITE_SUPABASE_ANON_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-// ------------------------------------
-
 createRoot(document.getElementById("root")!).render(<App />);
